7fd896d5890be64b70cbff3990a81e927ee42e50
Enzo Vezzaro <enzo-vezzaro@live.it>
2020-07-30 19:42:08 +0200

    added version and suppressed logs in prod

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let version = `
+  ********************************
+  ********************************
+  ***** SEARCH ALETHEIADATA ******
+  ************ v${'0.0.1'} **********
+  ********************************
+  ********************************
+  `;
+  console.log(version);
+
+if (window.location.host !== 'localhost:3000'){
+  console.log = function() {};
+  console.warn = function() {};
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
